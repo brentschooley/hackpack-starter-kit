884cff4c84bef3e77f807519f7a1f4a5e712afcb
Brent Schooley <schooleybrent@gmail.com>
2015-10-30 11:29:06 -0400

    Initial version

---
+++ hackpack-starter-kit/RGB.h
@@ -0,0 +1,9 @@
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
+
+// Define some colors we'll use frequently
+RGB white = { 255, 255, 255 };
+RGB off = { 0, 0, 0 };
---
+++ hackpack-starter-kit/hackpack-starter-kit.ino
@@ -0,0 +1,65 @@
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include "RGB.h"
+
+#define PIN 1
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
+  NEO_MATRIX_TOP + NEO_MATRIX_LEFT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800);
+
+
+void setup() {
+  matrix.begin();
+  matrix.setBrightness(30);
+  
+}
+
+void loop() {
+  
+}
+
+// Fill the pixels one after the other with a color
+void colorWipe(RGB color, uint8_t wait) {
+  for(uint16_t row=0; row < 8; row++) {
+    for(uint16_t column=0; column < 8; column++) {
+      matrix.drawPixel(column, row, matrix.Color(color.r, color.g, color.b));
+      matrix.show();
+      delay(wait);
+    }
+  }
+}
+
+// Fade pixel (x, y) from startColor to endColor
+void fadePixel(int x, int y, RGB startColor, RGB endColor, int steps, int wait) {
+  for(int i = 0; i <= steps; i++) 
+  {
+     int newR = startColor.r + (endColor.r - startColor.r) * i / steps;
+     int newG = startColor.g + (endColor.g - startColor.g) * i / steps;
+     int newB = startColor.b + (endColor.b - startColor.b) * i / steps;
+     
+     matrix.drawPixel(x, y, matrix.Color(newR, newG, newB));
+     matrix.show();
+     delay(wait);
+  }
+}
+
+void setPixel(int x, int y, RGB color) {
+  matrix.drawPixel(x, y, matrix.Color(color.r, color.g, color.b));
+}
+
+// Crossfade entire screen from startColor to endColor
+void crossFade(RGB startColor, RGB endColor, int steps, int wait) {
+  for(int i = 0; i <= steps; i++)
+  {
+     int newR = startColor.r + (endColor.r - startColor.r) * i / steps;
+     int newG = startColor.g + (endColor.g - startColor.g) * i / steps;
+     int newB = startColor.b + (endColor.b - startColor.b) * i / steps;
+     
+     matrix.fillScreen(matrix.Color(newR, newG, newB));
+     matrix.show();
+     delay(wait);
+  }
+}
